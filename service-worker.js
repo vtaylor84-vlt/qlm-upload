e3a2b5015003a9e448f3791c9fbfe8d02dc82ab9
vtaylor84-vlt <vtaylor84@gmail.com>
2025-11-11 17:20:09 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE = 'qlm-upload-v1';
+const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
